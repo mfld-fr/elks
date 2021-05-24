721b457aaba32c3425c04ffadbeead2baee86355
mfld-fr <github@mfld.fr>
2021-05-24 16:07:36 +0200

    Tune ROM configuration

--- emu86.sh
+++ emu86.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# Run ELKS in EMU86 (basic 8086 emulator)
+# Run ELKS in EMU86 (basic IA16 emulator)
 # EMU86 is part of the cross tools
+# See https://github.com/mfld-fr/emu86
 
-# ELKS must be configured minimaly with 'config-emu86'
-# with only core and using serial port for the console
+# For ELKS ROM Configuration:
+# ELKS must be configured minimaly with 'cp emu86-rom.config .config'
+# This uses headless console, HLT on idle, ROM filesystem.
 
-# First run ELKS with kernel and root FS in ROM
-# Kernel image @ segment 0xE000 (top of 1024K address space)
+# Kernel image @ segment 0xE000 (as 64K BIOS extension)
 # Root filesystem @ segment 0x8000 (assumes 512K RAM & 512K ROM)
 # Skip the INT 19h bootstrap in the kernel image (+0x42)
 
-emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin -x 0xe000:0x42 ${1+"$@"} &
+exec emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin ${1+"$@"}
 
-# Give time to EMU86 to emulate the serial port
-# and store the used PTY in 'emu86.pts'
+# For ELKS Full ROM Configuration:
+# ELKS must be configured minimaly with 'cp emu86-rom-full.config .config'
+# This adds MINIX/FAT filesytems with BIOS driver
 
-sleep 1
+exec emu86 -w 0xe0000 -f elks/arch/i86/boot/Image -w 0x80000 -f image/romfs.bin -I image/fd1440.bin ${1+"$@"}
 
-# Get the used PTY and redirect serial port to console
+# For ELKS disk image Configuration:
+# ELKS must be configured with 'cp emu86-disk.config .config'
+# This uses headless console, HLT on idle, no CONFIG_IDE_PROBE
 
-pcat `cat emu86.pts`
+exec emu86 -I image/fd1440.bin ${1+"$@"}
